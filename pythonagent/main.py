b26c4add72c3e2b11cda3fc41ba96caa691f75a8
Aryaman Gulati <gulatiaryaman18@gmail.com>
2025-10-31 15:11:00 +0400

    gemini api call

--- pythonagent/main.py
+++ pythonagent/main.py
@@ -1,20 +1,23 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
+    from google import genai
 
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Write a story about a cat.",
-)
-print(response.text)
-print(response.usage.model_tokens_details)
-print(response.usage.prompt_tokens_details)
-print(response.usage.completion_tokens_details)
-print(response.usage.total_tokens_details)
-print(response.usage.total_tokens)
-print(response.usage.total_tokens_details)
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Write a story about a cat.",
+    )
+    print(response.text)
+    if response is not None or response.usage_metadata is not None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("No usage metadata found")
+
+if __name__ == "__main__":
+    main()
