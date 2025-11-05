69e64a0c61d19ff816e23c7b0e64e2acc8e1973f
Aryaman Gulati <gulatiaryaman18@gmail.com>
2025-11-05 21:05:30 +0400

    add tests

--- pythonagent/tests.py
+++ pythonagent/tests.py
@@ -31,5 +31,6 @@ def main():
     print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
     print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
     print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print(write_file("calculator", "pkg/does_not_exist.py", "this should not be allowed"))
 
 main()
